36c9ec2b232f6bce5629c1a62a093c5dcf392802
jungsoo827 <jungsoo827@gmail.com>
2022-06-10 15:28:30 +0900

    SVY-210 added support for displaying page without questions

--- build.sh
+++ build.sh
@@ -3,32 +3,32 @@
 #npm install -g npm-cli-login
 npm-cli-login -u jenkins -p jenkins1234\!1 -e developers@coontec.com -r https://maven.meback.ai/repository/npm-private/
 
-npm install --registry https://maven.meback.ai/repository/npm-group
+npm install --registry https://maven.meback.ai/repository/npm-group/
 
 cd packages/survey-creator
 rm -rf build
-npm install --registry https://maven.meback.ai/repository/npm-group
+npm install --registry https://maven.meback.ai/repository/npm-group/
 npm run build
 cd build
 npm publish --registry https://maven.meback.ai/repository/npm-private/
 
 cd ../../survey-creator-core
 rm -rf build
-npm install --registry https://maven.meback.ai/repository/npm-group
+npm install --registry https://maven.meback.ai/repository/npm-group/
 npm run build
 cd build
 npm publish --registry https://maven.meback.ai/repository/npm-private/
 
 cd ../../survey-creator-knockout
 rm -rf build
-npm install --registry https://maven.meback.ai/repository/npm-group
+npm install --registry https://maven.meback.ai/repository/npm-group/
 npm run build
 cd build
 npm publish --registry https://maven.meback.ai/repository/npm-private/
 
 cd ../../survey-creator-react
 rm -rf build
-npm install --registry https://maven.meback.ai/repository/npm-group
+npm install --registry https://maven.meback.ai/repository/npm-group/
 npm run build
 cd build
 npm publish --registry https://maven.meback.ai/repository/npm-private/
